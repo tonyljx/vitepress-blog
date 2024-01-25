597753533094564cb5276f4ed1dd2c696a3aea9f
liangjiongxin <liangjiongxin@gmail.com>
2024-01-26 03:35:32 +0800

    feature: update homepage vue

--- .vitepress/theme/components/Home.vue
+++ .vitepress/theme/components/Home.vue
@@ -1,34 +1,32 @@
 <template>
+  <!-- panel -->
   <data-panel></data-panel>
 
+  <div class="home">
+    <div class="mx-auto container max-w-[1152px] min-h-[32px]  w-full rounded-[8px] p-[6px] mt-6">
+      <!-- bg-[var(--vp-c-bg-soft)] -->
+      <h2 class="mb-3 text-xl font-semibold">最近文章</h2>
+      <div class="flex flex-col gap-lg ">
+        <!-- grid grid-cols-3 -->
+        <div class="space-y-4" v-for="post in sidebarItems" :key="post.text">
+          <div class="flex flex-col gap-2">
+            <div class="flex items-center gap-2">
 
-  <div class="mx-auto container max-w-[1152px] min-h-[32px]  w-full rounded-[8px] p-[6px] mt-6">
-    <!-- bg-[var(--vp-c-bg-soft)] -->
-    <h2 class="mb-3 text-xl font-semibold">最近文章</h2>
-    <div class="flex flex-col gap-lg ">
-      <!-- panel -->
+              <div class="text-sm font-semibold text-gray-600">
+                #{{ post.categoryText }}
+              </div>
 
+              <div class="flex-grow text-blue-500 hover:text-blue-700 cursor-pointer transition duration-300"
+                @click="onClick(post.link)">
+                {{ post.text }}
+              </div>
 
-      <!-- grid grid-cols-3 -->
-      <div class="space-y-4" v-for="post in sidebarItems" :key="post.text">
-        <div class="flex flex-col gap-2">
-          <div class="flex items-center gap-2">
-
-            <div class="text-sm font-semibold text-gray-600">
-              #{{ post.categoryText }}
             </div>
-
-            <div class="flex-grow text-blue-500 hover:text-blue-700 cursor-pointer transition duration-300"
-              @click="onClick(post.link)">
-              {{ post.text }}
-            </div>
-
           </div>
         </div>
       </div>
+
     </div>
-
-
   </div>
 </template>
 
@@ -53,3 +51,23 @@ function onClick(url: string) {
 
 </script>
 
+<style scoped>
+.home {
+  padding: 0 24px;
+  margin-top: 24px;
+}
+
+@media (min-width: 640px) {
+  .home {
+    padding: 0 48px;
+    margin-top: 32px;
+  }
+}
+
+@media (min-width: 960px) {
+  .home {
+    padding: 0 64px;
+    margin-top: 48px;
+  }
+}
+</style>
